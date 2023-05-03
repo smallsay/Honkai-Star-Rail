174104ece1f955a5bc6bf0572e95b91747369041
BKX <791237835@qq.com>
2023-05-04 07:07:46 +0800

    Add files via upload

--- 星穹铁道短信聊天框/index.js
+++ 星穹铁道短信聊天框/index.js
@@ -5,32 +5,26 @@ var your_name = '娜塔莎';
 
 // 前面有空格是自己说话，没空格是对方说话
 var all_msg = [
-    '狗子在吗？',
-    ' 1',
+    '在吗？',
+    ' 我在',
     '最近地火发现一批形迹可疑的陌生人，在镇子里游荡，希儿将他们打晕了，从他们身上搜出了一张纸条',
     ' 纸条？',
-    '对，只有一张纸条，上面是一句奇怪的话：v我50',
+    '对，只有一张纸条，上面是一句意义不明的话：v我50',
     '对此你有什么头绪吗？',
-    ' 额...我想我大概知道是什么回事了，这事交给我',
+    ' ...我想我大概知道是什么回事了，这事交给我',
 ]
 
 
 
 
 
+var temp_my_msg = null;
+var temp_your_msg = null;
 
-
-
-
-
-window.addEventListener('load',function(){
-
+function set_msg() {
     var msg_end = document.querySelector('.msg_end');
-    var my_msg = document.querySelector('.my_msg');
-    var your_msg = document.querySelector('.msg');
     var chat_window = document.querySelector('.chat_window');
-    var temp_my_msg = my_msg.cloneNode(true);
-    var temp_your_msg = your_msg.cloneNode(true);
+    
     
     chat_window.innerHTML = '';
     
@@ -49,9 +43,21 @@ window.addEventListener('load',function(){
         chat_window.appendChild(temp_msg);
     }
     chat_window.appendChild(msg_end);
+}
 
 
-    
+
+
+
+
+window.addEventListener('load',function(){
+
+   
+    var my_msg = document.querySelector('.my_msg');
+    var your_msg = document.querySelector('.msg');
+  
+    temp_my_msg = my_msg.cloneNode(true);
+    temp_your_msg = your_msg.cloneNode(true);
 
 
     // 设置对方名字
@@ -68,6 +74,7 @@ window.addEventListener('load',function(){
 
     change_style();
 
+    set_msg();
 })
 
 
@@ -80,16 +87,50 @@ window.addEventListener('resize',function(){
 function change_style() {
     
     
-
     // 设置 msg_contine 的max-width
     var msg_contine = document.querySelectorAll('.msg_contine');
     var chat_window = document.querySelector('.chat_window');
     for(var i = 0 ; i < msg_contine.length ; i++) {
         msg_contine[i].style.maxWidth = chat_window.offsetWidth * 0.63 + 'px';
     }
-    
-
-
     chat_window.style.height = document.querySelector('.chat').offsetHeight * 0.9 + 'px';
 
 }
+
+
+
+// 设置聊天记录模块
+window.addEventListener('load',function(){
+
+    // hide button
+    var hide_button = document.querySelector('.set_text .hide');
+    hide_button.addEventListener('click',function(){
+        console.log(hide_button.parentElement.style.transform)
+        
+        if(hide_button.parentElement.style.transform != '' ) {
+            hide_button.parentElement.style.transform =  ""; 
+            hide_button.parentElement.style.opacity =  "1"; 
+        }else {
+            hide_button.parentElement.style.transform =  "translateY(-90%)"; 
+            hide_button.parentElement.style.opacity =  "0"; 
+        }
+    })
+
+    var all_text = document.querySelector('.set_text textarea');
+    this.setInterval(function(){
+        
+        var temp = all_text.value.split('\n');
+        
+        
+        all_msg = []
+        for(var i = 0 ; i < temp.length ; i++) {
+            if(temp[i]=='') continue;
+            else {
+                all_msg.push(temp[i])
+            }
+        }
+
+        set_msg();
+    },1000)
+
+})
